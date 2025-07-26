4f37358ba44dfe3d9608f90d118e479f200b4739
Stephane Mouako <stephane.mouako@sioux.eu>
2025-07-27 00:36:41 +0200

    add create_admin function to initialize admin user if not present.

--- src/core/config.py
+++ src/core/config.py
@@ -2,14 +2,17 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "sqlite+aiosqlite:///./ecommerce_dev.db"
+    DATABASE_URL: str 
     JWT_ACCESS_TOKEN_EXPIRE_SECONDS: int = 60
-    JWT_SECRET_KEY: str = "your_jwt_secret_key"
+    JWT_SECRET_KEY: str 
     JWT_ALGORITHM: str = "HS256"
     JWT_REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+    ADMIN_EMAIL: str 
+    ADMIN_PASSWORD: str 
+    ADMIN_FULL_NAME: str 
 
     class Config:
-        env_file = ".env"
+        env_file = "src/.env"
 
 
 settings = Settings()
--- src/database/core.py
+++ src/database/core.py
@@ -1,22 +1,32 @@
 from collections.abc import AsyncGenerator
-from sqlmodel import SQLModel
+from sqlmodel import SQLModel, select
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker
+from passlib.hash import bcrypt
 
+from src.users.schemas import UserRole
 from src.models import *  # noqa: F403
 from src.core.config import settings
+from src.models.user import Gender, User
 
 async_engine = create_async_engine(settings.DATABASE_URL, echo=True)
+
+async_session = sessionmaker(
+    bind=async_engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
 
 
 async def init_db():
     """Initialize the database by creating all tables."""
     async with async_engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
+        await create_admin()
 
 
-async def get_session() -> AsyncGenerator[AsyncSession, None, None]:
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
     """Get a session for database operations."""
 
     async_session = sessionmaker(
@@ -24,6 +34,24 @@ async def get_session() -> AsyncGenerator[AsyncSession, None, None]:
         class_=AsyncSession,
         expire_on_commit=False,
     )
-
     async with async_session() as session:
         yield session
+
+
+async def create_admin():
+    """Create an admin user if it does not already exist."""
+    async with async_session() as session:
+        # Check if any admin exists
+        existing = await session.exec(select(User).where(User.role == UserRole.admin))
+        if existing.first():
+            return
+
+        admin = User(
+            full_name=settings.ADMIN_FULL_NAME,
+            email=settings.ADMIN_EMAIL,
+            password_hash=bcrypt.hash(settings.ADMIN_PASSWORD),
+            gender=Gender.other,
+            role=UserRole.admin,
+        )
+        session.add(admin)
+        await session.commit()

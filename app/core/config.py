08faafea50a469a1699eb58f5ccc9563b93e82b5
Stephane Mouako <stephane.mouako@sioux.eu>
2025-07-28 15:47:19 +0200

    Fix formatting and update env_file path in configuration settings

--- app/core/config.py
+++ app/core/config.py
@@ -2,17 +2,17 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str 
+    DATABASE_URL: str
     JWT_ACCESS_TOKEN_EXPIRE_SECONDS: int = 60
-    JWT_SECRET_KEY: str 
+    JWT_SECRET_KEY: str
     JWT_ALGORITHM: str = "HS256"
     JWT_REFRESH_TOKEN_EXPIRE_DAYS: int = 7
-    ADMIN_EMAIL: str 
-    ADMIN_PASSWORD: str 
-    ADMIN_FULL_NAME: str 
+    ADMIN_EMAIL: str
+    ADMIN_PASSWORD: str
+    ADMIN_FULL_NAME: str
 
     class Config:
-        env_file = "src/.env"
+        env_file = "app/.env"
 
 
 settings = Settings()
